c8111b4f9b9adcf05f83ec396a9391ffb93f3a61
Isaac Chapman <isaac@isaacchapman.com>
2015-05-20 02:29:04 -0700

    properly kill selenium and ensure test exit code is passed to solano

--- tests/selftest.solano.sh
+++ tests/selftest.solano.sh
@@ -10,8 +10,11 @@ sleep 5
 
 # Run tests
 node_modules/.bin/intern-runner config=tests/selftest.solano.intern reporters=junit reporters=combined selftest=true selftest2 selftest3=a selftest3=b selftest3=c $@
+TEST_EXIT_CODE=$?
 
 # Stop selenium-standalone
 if [ -f "$TMPDIR/selenium-standalone.pid" ]; then
-  kill -9 $TMPDIR/selenium-standalone.pid
+  kill -9 `cat $TMPDIR/selenium-standalone.pid`
 fi
+
+exit $TEST_EXIT_CODE
